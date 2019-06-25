c527114356f6d279354bc1e475b1efc2d6d32c3d
Roflobster <seanmonrad@gmail.com>
2019-06-24 22:11:23 -0500

    Added use built in config file

--- src/main/java/me/roflobster/motd/motd.java
+++ src/main/java/me/roflobster/motd/motd.java
@@ -10,23 +10,36 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.io.File;
+
 // Implements Listener for events
 public class motd extends JavaPlugin implements Listener {
-    FileConfiguration config = getConfig();
+    //FileConfiguration config = getConfig();
+
+    public void registerConfig() {
+        this.getConfig().options().copyDefaults(true);
+        this.saveConfig();
+    }
 
 // Registers listener and posts in console that plugin activated
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
+        this.registerConfig();
+        //config.addDefault("playerOnJoin", "Installed Plugins: mcMMO, SimpleElevators, Dynmap, DiscordSRV, RampenDrills");
+
     }
+
 
 // Disables built in player join message in favor of custom one
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+        String pJoin = this.getConfig().getString("playerOnJoin");
         event.setJoinMessage(null);
 
         Bukkit.broadcastMessage(ChatColor.YELLOW + (player.getDisplayName() + " has joined the server"));
+        player.sendMessage(pJoin);
 
     }
 
@@ -39,8 +52,6 @@ public class motd extends JavaPlugin implements Listener {
         Bukkit.broadcastMessage(ChatColor.YELLOW + (player.getDisplayName() + " has left the server"));
 
     }
-
-
 
 
 
